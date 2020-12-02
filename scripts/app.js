275d19b6a70cf2e68f62eb70d6614e33c92a955c
toorajj <tooraj@hotmail.com>
2020-12-02 00:23:12 -0800

    style and js fun

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,31 @@
-console.log('Wake up Tomagotchi');
+
+$("#wake").on("click", function (event) {
+    console.warn("=== Keep Tomagotchi ALIVE !  ===");
+    startGame();
+
+});
+
+const startGame = function startGame() {
+    console.log('Call timer func');
+}
+
+$(".feed").on("click", function (event) {
+    console.warn("feed your pet!");
+    feedPet();
+
+});
+
+const feedPet = function feedPet() {
+    console.log('feed your pet');
+
+}
+$(".play").on("click", function (event) {
+    console.warn("play with your pet!");
+    playPet();
+
+});
+
+const playPet = function playPet() {
+    console.log('play with your pet');
+
+}
