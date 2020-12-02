4a8cc7994621ba279e60f31ff51111bdd938c6c1
toorajj <tooraj@hotmail.com>
2020-12-02 11:09:16 -0800

    time function

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,9 @@
 
+
 $("#wake").on("click", function (event) {
     console.warn("=== Keep Tomagotchi ALIVE !  ===");
     startGame();
+    setTime();
 
 });
 
@@ -29,3 +31,26 @@ const playPet = function playPet() {
     console.log('play with your pet');
 
 }
+
+
+
+let time = 300000;
+let age = 0;
+
+const setTime = function setTime() {
+
+    const updateTime = function updateTime() {
+        console.log("Time!", time);
+        $("h1").text(`Age : ${age} Time:${Math.round(time/60000)}`);
+        age++;
+        time=time-4000;
+       /* if (time <= 0) {
+            clearInterval(timer);
+            age++;
+            $("#round").text(`round: ${round}`);
+            if (age <= 4) setUpRound();
+        }*/
+    };
+    const timer = setInterval(updateTime, 4000);
+};
+
