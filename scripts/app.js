efcc2382f3c02c9bc62de9fa71b5bf78598270c8
toorajj <tooraj@hotmail.com>
2020-12-02 18:01:13 -0800

    worked on js file

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,8 @@
-
-
-
 $("#wake").on("click", function (event) {
     console.warn("=== Keep Tomagotchi ALIVE !  ===");
     startGame();
     setTime();
     hatch();
-    
-
 });
 
 const startGame = function startGame() {
@@ -27,6 +22,7 @@ const feedPet = function feedPet() {
 $(".play").on("click", function (event) {
     console.warn("play with your pet!");
     playPet();
+    play();
 
 });
 
@@ -51,14 +47,15 @@ const setTime = function setTime() {
         console.log("Time!", time); ///.pow .tofixed
         $("h1").text(`Age : ${age} Countdown: ${(time/60000).toFixed(2)}`);
         age++;
+        if (age > 2) { young(); }
         time = time - 4000;
         $("#hunger").text(`Hunger: ${hunger} `);
-        hunger-=3;
+        hunger -= 3;
         $("#sleep").text(`Sleepiness: ${sleep} `);
-        sleep-=2;
+        sleep -= 2;
         $("#boredom").text(`Boredom: ${bordom} `);
         bordom--;
-        if (time <= 0 || hunger <= 0 || sleep <= 0 || bordom <= 0 ) {
+        if (time <= 0 || hunger <= 0 || sleep <= 0 || bordom <= 0) {
             killThePet();
             $("h1").text(`Age : ${age} Tomagotchi Died`);
             clearInterval(timer);
@@ -71,16 +68,27 @@ const setTime = function setTime() {
 const killThePet = function killThePet() {
 
     console.log('killed pet');
+    $(".tomagotchi").fadeOut(3000);
+};
+
+const hatch = function hatch() {
+    $(".tomagotchi").fadeIn(5000).html("<img src='./img/hatch.png' alt='Hatched Egg' width='200' height='300' >").fadeOut(2000);
+   // $(".tomagotchi").css();
+};
+const young = function young(){
+    $(".tomagotchi").fadeIn(1000).html("<img src='./img/young.png' alt='young rooster' width='200' height='300' >");
+};
+
+const play = function play(){
+    $(".tomagotchi").css('transform', 'scale(1.1)');
+    bordom-=10;
 };
 
 
-/*const char = function char () {
 
-    $(".tomagotchi").html("<img src='./img/egg1.png' alt='Egg image' width='200' height='300' >");
-}
-char();*/
+/*const light = function light() {
+        $(".light").on("click", function (event) {
 
-const hatch = function hatch () {
-    $(".tomagotchi").html("<img src='./img/hatch.png' alt='Hatched Egg' width='200' height='300' >");
-}
 
+
+        });*/
